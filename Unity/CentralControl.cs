a36688f0f01f3a2e4e2775f2c668505073ed314e
Ian Kerins <ianskerins@gmail.com>
2014-04-18 12:27:35 -0400

    Finish CPG model in C# except file IO

--- Unity/CentralControl.cs
+++ Unity/CentralControl.cs
@@ -7,20 +7,20 @@ public class CentralControl : MonoBehaviour {
     // All angular values are in radians
     private int n;
     private float[] a, theta, ampl, ampl_dot, signal;
+    // Evolved values for the limb setpoint function
+    private float[] gsl, gsh, gb1, gb2;
     // 0 for body, 1 for limb
     private int[] osc_class;
     private float[,] w, phi;
      
     // Parameters defined by table S1 (body, limb)
-    private float[] cv0;
-    private float[] cv1;
-    private float[] cR0;
-    private float[] cR1;
+    private float[] cv0, cv1, cR0, cR1;
     // ( (dbody_low, dbody_high), (dlimb_low, dlimb_high) )
     private int[,] d_params;
 
     public CPG(int n, float[] a, float[] theta, float[] ampl, float[] ampl_dot, 
-               int[] osc_class, float[,] w, float[,] phi){
+               int[] osc_class, float[,] w, float[,] phi, float[] gsl, 
+               float[] gsh, float[] gb1, float[] gb2){
       cv0 = new float[] {0.3f, 0.0f};
       cv1 = new float[] {0.2f, 0.2f};
       cR0 = new float[] {0.196f, 0.131f};
@@ -35,6 +35,10 @@ public class CentralControl : MonoBehaviour {
       this.osc_class = osc_class;
       this.w = w;
       this.phi = phi;
+      this.gsl = gsl;
+      this.gsh = gsh;
+      this.gb1 = gb1;
+      this.gb2 = gb2;
     }
     
     // Calculate drives 
@@ -110,6 +114,11 @@ public class CentralControl : MonoBehaviour {
     public float get_body_setpoint(float alpha, int l_index, int r_index){
       return alpha*(signal[l_index] - signal[r_index]);
     }
+
+    public float get_limb_setpoint(int i){
+      return (theta[i] >= gb1[i] && theta[i] <= gb2[i]) ? gsh[i] * theta[i] : 
+        gsl[i] * theta[i];
+    }
   }
 
   // Joints and Oscillators can be indexed by their number on the 
@@ -125,6 +134,17 @@ public class CentralControl : MonoBehaviour {
 
   // Initialization of variables
   void Start () {
+    int n = 20;
+    int gen = 0;
+    // Initialize the whole CPG structure
+    float[] a, theta, ampl, ampl_dot, gsl, gsh, gb1, gb2;
+    int[] osc_class;
+    float[,] w, phi;
+    getParams(n, out a, out theta, out ampl, out ampl_dot, out osc_class, 
+              out w, out phi, out gsl, out gsh, out gb1, out gb2);
+    salamander = new CPG(n, a, theta, ampl, ampl_dot, osc_class, w, phi, gsl, 
+                         gsh, gb1, gb2);
+    
     // Some segments have only one joint
     joints[0] = GameObject.Find("2").GetComponent<HingeJoint>();
     joints[1] = GameObject.Find("3").GetComponent<HingeJoint>();
@@ -135,7 +155,8 @@ public class CentralControl : MonoBehaviour {
     // Others have more than one joint, and these must be differentiated
     HingeJoint[] feet = GameObject.Find("1/Center").GetComponents<HingeJoint>();
     foreach(HingeJoint foot in feet){
-      if(foot.connectedBody == GameObject.Find("1/LeftFoot").GetComponent<Rigidbody>()){
+      if(foot.connectedBody == 
+         GameObject.Find("1/LeftFoot").GetComponent<Rigidbody>()){
         joints[6] = foot;
       }
       else{ 
@@ -144,10 +165,12 @@ public class CentralControl : MonoBehaviour {
     }
     feet = GameObject.Find("4/Center").GetComponents<HingeJoint>();
     foreach(HingeJoint foot in feet){
-      if(foot.connectedBody == GameObject.Find("4/LeftFoot").GetComponent<Rigidbody>()){
+      if(foot.connectedBody == 
+         GameObject.Find("4/LeftFoot").GetComponent<Rigidbody>()){
         joints[8] = foot;
       }
-      else if(foot.connectedBody == GameObject.Find("4/RightFoot").GetComponent<Rigidbody>()){
+      else if(foot.connectedBody == 
+              GameObject.Find("4/RightFoot").GetComponent<Rigidbody>()){
         joints[9] = foot;
       }
       // Also have to check for segment in front of this segment 
@@ -157,20 +180,61 @@ public class CentralControl : MonoBehaviour {
     }
   }
 
-  // This function is called every .2 seconds of simulation, and is where motor changes will happen
+  // This function is called every .2 seconds of simulation, and is where motor 
+  // changes will happen
   void FixedUpdate(){
+    // We aren't actually using time yet . . .
     float time = Time.fixedTime;
-    foreach (HingeJoint joint in joints) {
+    float stepTime = Time.deltaTime;
+    float desiredAngle;
+    float alpha = 0.5f;
+    // Body joints
+    for(int i = 0; i < 6; i++){
       JointMotor m = new JointMotor();
-      m.force = joint.motor.force;
-      m.freeSpin = joint.motor.freeSpin;
-      if(joint.connectedBody.gameObject.name == "LeftFoot" || joint.connectedBody.gameObject.name == "RightFoot"){
-        m.targetVelocity = rand.Next(-250, 251);
-      }
-      else{
-        m.targetVelocity = rand.Next(-40, 41);
-      }
-      joint.motor = m;
+      m.force = joints[i].motor.force;
+      m.freeSpin = joints[i].motor.freeSpin;
+      desiredAngle = (i < 3) ? salamander.get_body_setpoint(alpha, i+1, i+9) : 
+        salamander.get_body_setpoint(alpha, i+2, i+10);
+      desiredAngle *= Mathf.Rad2Deg;
+      m.targetVelocity = (desiredAngle - joints[i].angle) / stepTime;
+      joints[i].motor = m;
+      alpha += 0.5f/6.0f;
+    }   
+    // Limb joints
+    for(int i = 6; i < 10; i++){
+      JointMotor m = new JointMotor();
+      m.force = joints[i].motor.force;
+      m.freeSpin = joints[i].motor.freeSpin;      
+      desiredAngle = salamander.get_limb_setpoint(i+10) * Mathf.Rad2Deg;
+      m.targetVelocity = (desiredAngle - joints[i].angle) / stepTime;
+      joints[i].motor = m;
     }
+  }
+
+  void getParams(int n, out float[] a, out float[] theta, out float[] ampl, 
+                 out float[] ampl_dot, out int[] osc_class, out float[,] w, 
+                 out float[,] phi, out float[] gsl, out float[] gsh, 
+                 out float[] gb1, out float[] gb2){
+    
+    a = new float[n]; 
+    theta = new float[n]; 
+    ampl = new float[n]; 
+    ampl_dot = new float[n];
+    osc_class = new int[n];
+    w = new float[n,n]; 
+    phi = new float[n,n];
+    gsl = new float[n];
+    gsh = new float[n];
+    gb1 = new float[n];
+    gb2 = new float[n];
+    // Set up intrinsic value: osc_class
+    for(int i = 0; i < 16; i ++){
+      osc_class[i] = 0;
+    }
+    for(int i = 16; i < 20; i++){
+      osc_class[i] = 1;
+    }
+    
+    // Read in gen values: a, theta, ampl, ampl_dot, w, phi, G
   }
 }
